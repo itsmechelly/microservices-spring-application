d1ba47733791cac68d3e60e1884c17c1ed6ae5e0
Chelly izraelov <hlly.lov@gmail.com>
2021-11-04 19:12:46 +0200

    ...

--- service-a/src/main/java/app/core/ServiceAApplication.java
+++ service-a/src/main/java/app/core/ServiceAApplication.java
@@ -5,15 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-//@EnableFeignClients
-//@EnableHystrix
+@EnableFeignClients
+@EnableHystrix
 public class ServiceAApplication {
 
 	public static void main(String[] args) {
