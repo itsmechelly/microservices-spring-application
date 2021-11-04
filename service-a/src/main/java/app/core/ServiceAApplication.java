be5d4ed82ec7c06c405de74eba7cd331b4f92b32
Chelly izraelov <hlly.lov@gmail.com>
2021-11-04 12:47:15 +0200

    ...

--- service-a/src/main/java/app/core/ServiceAApplication.java
+++ service-a/src/main/java/app/core/ServiceAApplication.java
@@ -2,14 +2,28 @@ package app.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+//@EnableFeignClients
+//@EnableHystrix
 public class ServiceAApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- service-a/src/main/java/app/core/web/ControllerA.java
+++ service-a/src/main/java/app/core/web/ControllerA.java
@@ -1,13 +1,72 @@
 package app.core.web;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+//Note - in comments - how Consul runs behind the scenes.
 
 @RestController
 public class ControllerA {
 
+	@Autowired
+	private RestTemplate template;
+//	@Autowired
+//	private DiscoveryClient discoveryClient;
+//	@Autowired
+//	private LoadBalancerClient lb;
+
 	@GetMapping("/service/a")
-	public String haandleA() {
-		return "Service A";
+	@HystrixCommand(fallbackMethod = "handleAFallback")
+	public String handleA() {
+		String url = "http://service-b/service/b";
+		return "Service A calling B: " + template.getForObject(url, String.class);
 	}
+
+	public String handleAFallback(Throwable t) {
+		return "Service A Fallback message: Can't call service B. cause:" + t;
+	}
+
+//	@GetMapping("/service/a")
+//	public String handleA() {
+//		try {
+//			// get the base URI of a service B instance
+//			String serviceId = "service-b";
+//			URI baseUri = getServiceInstanceBaseUriLB(serviceId);
+//			String url = baseUri + "/service/b";
+//			System.out.println(url);
+//			// make the request
+//			return "Service A calling B: " + template.getForObject(url, String.class);
+//		} catch (Exception e) {
+//			e.printStackTrace(System.out);
+//			return "Error";
+//		}
+//	}
+//
+//	private URI getServiceInstanceBaseUriLB(String serviceId) {
+//		return lb.choose(serviceId).getUri();
+//	}
+//
+//	private URI getServiceInstanceBaseUri(String serviceId) {
+//		// print the service id
+//		System.out.println("service id: " + serviceId);
+//		// print all of the available instances of the service
+//		System.out.println("service instance:");
+//		List<ServiceInstance> serviceInstances = discoveryClient.getInstances(serviceId);
+//		for (ServiceInstance serviceInstance : serviceInstances) {
+//			System.out.println(serviceInstance);
+//		}
+//		// return the URI of the first instance (NO LB)
+//		return serviceInstances.get(0).getUri();
+//	}
+
 }
